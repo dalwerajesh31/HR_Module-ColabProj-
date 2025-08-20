9ccab3d03b24d9e521af856a573139915de81001
dalwerajesh31 <dalwerajesh31@gmail.com>
2025-08-21 00:30:13 +0530

    New Page Master Department is added with full working and tested well.

--- HR_Module_Proj/Data/ApplicationDbContext.cs
+++ HR_Module_Proj/Data/ApplicationDbContext.cs
@@ -8,6 +8,22 @@ namespace HR_Module_Proj.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-        DbSet<Staffs> MasterStaffDetails { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MasterDepartment>(entity =>
+            {
+               entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.DepartmentName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.ShortName).IsRequired().HasMaxLength(5);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.UpdatedAt).HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.IsActive).HasDefaultValue(true);
+            });
+                
+        }
+        public DbSet<Staffs> MasterStaffDetails { get; set; } = null!;
+        public DbSet<MasterDepartment> MasterDepartments { get; set; } = null!;    
     }
 }
