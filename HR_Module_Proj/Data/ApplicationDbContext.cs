bd9c4c59bfeee0bcb255f8e74076a3cc9268c6d3
dalwerajesh31 <dalwerajesh31@gmail.com>
2025-08-27 22:46:39 +0530

    Changes pushed on date 28-08-2025.

--- HR_Module_Proj/Data/ApplicationDbContext.cs
+++ HR_Module_Proj/Data/ApplicationDbContext.cs
@@ -8,12 +8,12 @@ namespace HR_Module_Proj.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-       
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<MasterDepartment>(entity =>
             {
-               entity.HasKey(e => e.Id);
+                entity.HasKey(e => e.Id);
 
                 entity.Property(e => e.DepartmentName).IsRequired().HasMaxLength(50);
                 entity.Property(e => e.ShortName).IsRequired().HasMaxLength(5);
@@ -21,10 +21,21 @@ namespace HR_Module_Proj.Data
                 entity.Property(e => e.UpdatedAt).HasDefaultValueSql("GETUTCDATE()");
                 entity.Property(e => e.IsActive).HasDefaultValue(true);
             });
-                
+
+            modelBuilder.Entity<MasterRoles>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.RoleName).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.ShortName).IsRequired().HasMaxLength(5);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.UpdatedAt).HasDefaultValueSql("GETUTCDATE()");
+                entity.Property(e => e.IsActive).HasDefaultValue(true);
+            });
         }
         public DbSet<Staffs> MasterStaffDetails { get; set; } = null!;
         public DbSet<MasterDepartment> MasterDepartments { get; set; } = null!;
-		public DbSet<MasterDesignation> MasterDesignation { get; set; } = null!;
-	}
+        public DbSet<MasterDesignation> MasterDesignation { get; set; } = null!;
+        public DbSet<MasterRoles> MasterRoles { get; set; } = null!;
+    }
 }
---
+++ HR_Module_Proj/Models/MasterRoleVM.cs
@@ -0,0 +1,8 @@
+﻿namespace HR_Module_Proj.Models
+{
+    public class MasterRoleVM
+    {
+        public Models.Entities.MasterRoles role { get; set; } = new Models.Entities.MasterRoles();
+        public List<Models.Entities.MasterRoles> roles { get; set; } = new List<Models.Entities.MasterRoles>();
+    }
+}
