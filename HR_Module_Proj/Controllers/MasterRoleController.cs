952535ae065f2d4ebf942a3bd08605991f7db43d
dalwerajesh31 <dalwerajesh31@gmail.com>
2025-08-30 00:07:50 +0530

    Changes in MasterRole Controller.

--- HR_Module_Proj/Controllers/MasterRoleController.cs
+++ HR_Module_Proj/Controllers/MasterRoleController.cs
@@ -1,4 +1,5 @@
 ﻿using HR_Module_Proj.Models;
+using HR_Module_Proj.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,6 +19,44 @@ namespace HR_Module_Proj.Controllers
             model.roles = await _context.MasterRoles.ToListAsync();
             return View(model);
             //return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SaveAndUpdate([Bind(Prefix = "department")] MasterRoles model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id == 0)
+                {
+                    //Create new department
+                    _context.MasterRoles.Add(model);
+                }
+                else
+                {
+                    //Update existing department
+                    var existingDepartment = await _context.MasterRoles.FindAsync(model.Id);
+                    if (existingDepartment == null)
+                    {
+                        return NotFound();
+                    }
+                    existingDepartment.RoleName = model.RoleName;
+                    existingDepartment.ShortName = model.ShortName;
+                    existingDepartment.IsActive = model.IsActive;
+                    existingDepartment.UpdatedAt = DateTime.UtcNow;
+                    _context.MasterRoles.Update(existingDepartment);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                var vmInValid = new MasterRoleVM
+                {
+                    role = model,
+                    roles = await _context.MasterRoles  .ToListAsync()
+                };
+                return View("Index", vmInValid);
+            }
         }
     }
 }
